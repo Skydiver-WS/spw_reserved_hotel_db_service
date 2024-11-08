087eae79dc41d2e6dae2319749ca79cf9382adfc
Skydiver-WS <isu172@mail.ru>
2024-11-09 00:58:18 +0300

    Create docker run script

--- .github/workflows/docker-run.sh
+++ .github/workflows/docker-run.sh
@@ -1,2 +1,5 @@
+docker rm -f db_service
+
 docker push skydiverkhv/db_service:latest
-docker run -d -p 8080:8080 skydiverkhv/db_service:latest --name db_service
+
+docker run -d --name db_service --network kafka_app-network -p 8080:8080 -e SPRING_KAFKA_BOOTSTRAP_SERVERS=kafka:9092 skydiverkhv/db_service:latest
