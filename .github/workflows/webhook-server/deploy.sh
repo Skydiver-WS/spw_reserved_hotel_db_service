b4ed00c3448b5738d3005c9c9b4044bfa76634d8
Skydiver-WS <isu172@mail.ru>
2024-11-08 23:16:12 +0300

    fixed docker run

--- .github/workflows/webhook-server/deploy.sh
+++ .github/workflows/webhook-server/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Pull the latest image from Docker Hub
-docker pull skydiver_ws/db_service:latest
+docker pull skydiverkhv/db_service:latest
 
 # Stop and remove the current container
 docker stop app_container || true
 docker rm app_container || true
 
 # Start a new container with the updated image
-docker run -d --name app_container -p 3000:3000 skydiver_ws/db_service:latest
+docker run -d --name app_container -p 3000:3000 skydiverkhv/db_service:latest
