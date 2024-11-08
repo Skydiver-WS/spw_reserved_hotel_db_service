5c3cf235f2c278daad0b259d52298c2666feb4da
Skydiver-WS <isu172@mail.ru>
2024-11-09 01:18:53 +0300

    Fixed adress kafka

--- .github/workflows/docker-run.sh
+++ .github/workflows/docker-run.sh
@@ -2,4 +2,4 @@ docker rm -f db_service
 
 docker push skydiverkhv/db_service:latest
 
-docker run -d --name db_service --network kafka_app-network -p 8080:8080 -e SPRING_KAFKA_BOOTSTRAP_SERVERS=127.0.1.2:9092 skydiverkhv/db_service:latest
+docker run -d --name db_service --network kafka_app-network -p 8080:8080 -e SPRING_KAFKA_BOOTSTRAP_SERVERS=kafka:9092 skydiverkhv/db_service:latest
