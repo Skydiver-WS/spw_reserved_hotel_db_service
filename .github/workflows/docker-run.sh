f9de2354008f9ec51216073999e3ed3f408fd71d
Skydiver-WS <isu172@mail.ru>
2025-02-06 21:43:17 +0300

    Fixed kafka connect

--- .github/workflows/docker-run.sh
+++ .github/workflows/docker-run.sh
@@ -1,5 +1,5 @@
 docker rm -f db_service
 
-docker push skydiverkhv/db_service:v1.0.5
+docker push skydiverkhv/db_service:v1.0.6
 
-docker run -d --name db_service --network kafka_app-network -p 8080:8080 -e SPRING_KAFKA_BOOTSTRAP_SERVERS=kafka:9092 skydiverkhv/db_service:v1.0.5
+docker run -d --name db_service --network kafka_app-network -p 8080:8080 -e SPRING_KAFKA_BOOTSTRAP_SERVERS=kafka:9092 skydiverkhv/db_service:v1.0.6
