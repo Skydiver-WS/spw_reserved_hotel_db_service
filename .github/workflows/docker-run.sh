6d4b647014b6d4760a8a022322bc340b0231dfca
Skydiver_WS <isu172@mail.ru>
2025-09-20 18:48:51 +0300

    Fix auto version ci/cd

--- .github/workflows/docker-run.sh
+++ .github/workflows/docker-run.sh
@@ -1,5 +1,10 @@
+# Достаём версию из pom.xml
+VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+
+echo "Using version: $VERSION"
+
 docker rm -f db_service
 
-docker push skydiverkhv/db_service:v1.1.1
+docker push skydiverkhv/db_service:v"$VERSION"
 
-docker run -d --name db_service --network skydiverkhv/db_service:v1.1.1
+docker run -d --name db_service --network skydiverkhv/db_service:"$VERSION"
